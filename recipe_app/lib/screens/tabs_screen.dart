878abb9788492ee94e57361d373863a92e428d92
SheikhAman <amansk664@gmail.com>
2022-05-27 02:48:41 +0600

    recipe_app

---
+++ recipe_app/lib/screens/tabs_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import '../screens/favorites_screen.dart';
+import '../screens/categories_screen.dart';
+import '../widgets/main_drawer.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+
+  const TabsScreen(this.favoriteMeals, {Key? key}) : super(key: key);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  late List<Map<String, Object>> _pages;
+
+  @override
+  void initState() {
+    _pages = [
+      {'page': CategoriesScreen(), 'title': 'Categories'},
+      {'page': FavoritesScreen(widget.favoriteMeals), 'title': 'Your Favorite'}
+    ];
+    super.initState();
+  }
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title'].toString()),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.pink,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).colorScheme.secondary,
+          onTap: _selectPage,
+          currentIndex: _selectedPageIndex,
+          selectedFontSize: 14,
+          unselectedFontSize: 14,
+          // type: BottomNavigationBarType.shifting,
+          items: [
+            BottomNavigationBarItem(
+                backgroundColor: Colors.pink,
+                icon: Icon(Icons.category),
+                label: 'Categories'),
+            BottomNavigationBarItem(
+                backgroundColor: Colors.pink,
+                icon: Icon(Icons.star),
+                label: 'Favorites'),
+          ]),
+    );
+  }
+}
